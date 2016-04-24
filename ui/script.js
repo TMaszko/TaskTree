74a26f76cca032f1d1b809d2c763af749cf2c848
Tomasz Krzyżowski <t.krzyzowski96@gmail.com>
2016-04-24 22:49:07 +0200

    Adding tasks feature

--- ui/script.js
+++ ui/script.js
@@ -6,16 +6,33 @@
     var hide = document.querySelector('.hideAll');
     var taskBox = document.querySelector('.taskBox');
     var taskList = new TaskList(list);
+    var newTask = document.querySelector('#newTask');
+    var OkBtn = document.querySelector('#okTask');
+    var taskBoxForm = document.querySelector('.taskBoxForm');
     btn.addEventListener('click',function(){
       if(taskList.isEmpty()){
-        msg.classList.toggle('is-hidden');
+        msg.classList.add('is-hidden');
       }
-      hide.classList.toggle('fadeIn');
-      taskBox.classList.toggle('is-hidden');
-      taskList.addTask();
+      else msg.classList.toggle('is-hidden');
+      hide.classList.add('fadeIn');
+      hide.classList.remove('is-hidden');
+      taskBox.classList.remove('is-hidden');
+      newTask.focus();
     });
-    hide.addEventListener('click',function(){
+      taskBoxForm.addEventListener('submit',function(e){
+        e.preventDefault();
+      });
+          taskBox.addEventListener('click',function(e){
+          if(e.target == OkBtn){
+            taskList.addTask(newTask.value);
+          }
+          });
+
+
+
+    hide.addEventListener('click',function(){ //ukrywanie elementu po najechaniu na cos innego niz taskBox
       this.classList.remove('fadeIn');
+      this.classList.add('is-hidden');
       taskBox.classList.add('is-hidden');
     })
   });
