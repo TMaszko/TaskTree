d05de5db67e43ef11777429fccffb44a79c077da
Tomasz Krzyżowski <t.krzyzowski96@gmail.com>
2016-04-24 21:37:00 +0200

    fade in and taskBox

--- ui/script.js
+++ ui/script.js
@@ -3,13 +3,22 @@
     var list = document.querySelector('#tasks');
     var btn = document.querySelector('.plusBtn');
     var msg = document.querySelector('.messageEmptyList');
+    var hide = document.querySelector('.hideAll');
+    var taskBox = document.querySelector('.taskBox');
     var taskList = new TaskList(list);
     btn.addEventListener('click',function(){
       if(taskList.isEmpty()){
-        msg.classList.toggle('messageEmptyList-hidden');
+        msg.classList.toggle('is-hidden');
       }
-      taskList.addTask('Jajko');
+      hide.classList.toggle('fadeIn');
+      taskBox.classList.toggle('is-hidden');
+      taskList.addTask();
     });
+    hide.addEventListener('click',function(){
+      this.classList.remove('fadeIn');
+      taskBox.classList.add('is-hidden');
+    })
   });
+
 
 })();
