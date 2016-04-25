c0861d2e64808e779ebebd420a9aaeb1f135edec
Tomasz Krzyżowski <t.krzyzowski96@gmail.com>
2016-04-25 13:55:30 +0200

    Deleting elements feature

--- ui/script.js
+++ ui/script.js
@@ -4,7 +4,8 @@
 
 /* -------------------GLOBAL VARIABLES-------------------------------------*/
     var list = document.querySelector('#tasks');
-    var btn = document.querySelector('.plusBtn');
+    var plusBtn = document.querySelector('.plusBtn');
+    var trashIcon = document.querySelector('.trashBtn');
     var msg = document.querySelector('.messageEmptyList');
     var hide = document.querySelector('.hideAll');
     var taskBox = document.querySelector('.taskBox');
@@ -15,28 +16,62 @@
     var taskBoxForm = document.querySelector('.taskBoxForm');
 
 /* --------------------------------------------------------*/
-    btn.addEventListener('click',function(){
+    plusBtn.addEventListener('click',function(){
       if(taskList.isEmpty()){
         msg.classList.add('is-hidden');
       }
-      hide.classList.add('fadeIn');
-      hide.classList.remove('is-hidden');
-      taskBox.classList.remove('is-hidden');
+      showTaskBox.call(hide);
       newTask.focus();
     });
+    function showTaskBox(){
+      this.classList.add('fadeIn');
+      this.classList.remove('is-hidden');
+      taskBox.classList.remove('is-hidden');
+    }
     function hideTaskBox(){
           this.classList.remove('fadeIn');
           this.classList.add('is-hidden');
           taskBox.classList.add('is-hidden');
     }
+
       taskBoxForm.addEventListener('submit',function(e){
         e.preventDefault();
+        newTask.blur();
       });
+
+      trashIcon.addEventListener('click',function(){
+        var deleted = document.querySelectorAll('.active');
+        Array.prototype.forEach.call(deleted,function(el){
+          taskList.removeTask(el);
+        });
+          plusBtn.classList.remove('is-hidden');
+          trashIcon.classList.add('is-hidden');
+      });
+
           taskBox.addEventListener('click',function(e){
             if(e.target === okBtn){
               taskList.addTask(newTask.value);
               hideTaskBox.call(hide);
               newTask.value = '';
+              var tasks = document.querySelectorAll('.task');
+                tasks[tasks.length -1].addEventListener('click',function(){
+                  this.classList.toggle('active');
+                  var tasks = document.querySelectorAll('.task'); // aktualna lista
+                  var passed = Array.prototype.some.call(tasks,function(item){
+                    console.log(item.classList);
+                    return item.classList.contains('active');
+                  });
+                  if(passed){
+                    plusBtn.classList.add('is-hidden');
+                    trashIcon.classList.remove('is-hidden');
+                  }
+                  else if(Array.prototype.every.call(tasks,function(item){
+                    return !item.classList.contains('active');
+                  })){
+                    plusBtn.classList.remove('is-hidden');
+                    trashIcon.classList.add('is-hidden');
+                  }
+                });
             }
             else if(e.target === cancelTask){
               hideTaskBox.call(hide);
@@ -48,8 +83,11 @@
 
     hide.addEventListener('click',function(){ //ukrywanie elementu po najechaniu na cos innego niz taskBox
         hideTaskBox.call(this);
-    })
+        newTask.value = '';
+    });
+
   });
+
 
 
 })();
--- ui/taskList.js
+++ ui/taskList.js
@@ -3,9 +3,13 @@ function TaskList(list){
   this.addTask = function(title){
     var li = document.createElement('li');
     var text = document.createTextNode(title);
+    li.classList.add('task');
     li.appendChild(text);
     list.appendChild(li);
   };
+  this.removeTask = function(el){
+    list.removeChild(el);
+  }
   this.getList = function(){
     return list;
   }
